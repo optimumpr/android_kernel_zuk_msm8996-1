796abc4539e987e20587d58fffc010205104ecb8
jollaman999 <admin@jollaman999.com>
2019-05-29 10:24:25 -0400

    Add and enable MSM hotplug

--- drivers/fingerprint/fpc1020_ree.c
+++ drivers/fingerprint/fpc1020_ree.c
@@ -29,12 +29,21 @@
 #include <linux/of_gpio.h>
 #include <linux/input.h>
 
+#ifdef CONFIG_MSM_HOTPLUG
+#include <linux/msm_hotplug.h>
+#include <linux/workqueue.h>
+#endif
+
 #define FPC1020_TOUCH_DEV_NAME  "fpc1020tp"
 
 #define FPC1020_RESET_LOW_US 1000
 #define FPC1020_RESET_HIGH1_US 100
 #define FPC1020_RESET_HIGH2_US 1250
 #define FPC_TTW_HOLD_TIME 1500
+
+#ifdef CONFIG_MSM_HOTPLUG
+extern void msm_hotplug_resume_timeout(void);
+#endif
 
 struct fpc1020_data {
 	struct device   *dev;
@@ -251,6 +260,14 @@ static const struct attribute_group attribute_group = {
 	.attrs = attributes,
 };
 
+#ifdef CONFIG_MSM_HOTPLUG
+static void msm_hotplug_resume_call(struct work_struct *msm_hotplug_resume_call_work)
+{
+	msm_hotplug_resume_timeout();
+}
+static DECLARE_WORK(msm_hotplug_resume_call_work, msm_hotplug_resume_call);
+#endif
+
 static void fpc1020_report_work_func(struct work_struct *work)
 {
 	struct fpc1020_data *fpc1020 = NULL;
@@ -324,6 +341,13 @@ static irqreturn_t fpc1020_irq_handler(int irq, void *_fpc1020)
 	if (fpc1020->wakeup_enabled && !fpc1020->screen_on) {
 		pm_wakeup_event(fpc1020->dev, 5000);
 	}
+#ifdef CONFIG_MSM_HOTPLUG
+	if (msm_enabled && msm_hotplug_scr_suspended &&
+	   !msm_hotplug_fingerprint_called) {
+		msm_hotplug_fingerprint_called = true;
+		schedule_work(&msm_hotplug_resume_call_work);
+	}
+#endif
 	sysfs_notify(&fpc1020->dev->kobj, NULL, dev_attr_irq.attr.name);
 	return IRQ_HANDLED;
 }
